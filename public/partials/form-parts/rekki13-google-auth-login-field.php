8232b819c524a228b816202cd667f0c683954eb0
Danil <61312812+rekki13@users.noreply.github.com>
2023-02-13 11:47:41 +0200

    Create rekki13-google-auth-login-field.php

---
+++ public/partials/form-parts/rekki13-google-auth-login-field.php
@@ -0,0 +1,5 @@
+<div class="login-googleotp-wrapper">
+	<label for="googleotp"><?= __( 'Google2AF code', 'rekki13-google-auth' ) ?></label>
+	<input type="text" tabindex="20" size="20" value="" class="input" id="googleotp" name="googleotp"
+	<small><?= __( 'If you don\'t have Google Authenticator enabled for your WordPress account, leave this field empty.', 'rekki13-google-auth' ) ?></small>
+</div>
